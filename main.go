6a14ea4ddb9f8457c0fb09d96323df80e2215e38
rankrev <zq_zhouqi@qq.com>
2023-03-21 14:45:27 +0800

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func handler(conn net.Conn) {
+    defer conn.Close()
+    reader:=bufio.NewReader(conn)
+    writer:=bufio.NewWriter(conn)
+    rw:=bufio.NewReadWriter(reader, writer)
+    request_line, _:=rw.ReadString('\n')
+    response:=fmt.Sprintf("Server got message: %s", request_line)
+    rw.Write([]byte(response))
+}
+
+func main() {
+    ln, err:=net.Listen("tcp", ":8080")
+    if err!=nil {
+        panic(err)
+    }
+    
+    for {
+        conn, err:=ln.Accept()
+        if err!=nil {
+            log.Println("Can't have connection\n", conn)
+        }
+
+        go handler(conn)
+    }
+}
